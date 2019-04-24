74f7a572e8e5019ba2f2ae0634a876595f5847ac
Vladzimir Yasinsky <yasinsky2015@gmail.com>
2019-04-24 23:05:09 +0300

    delete console.log

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,3 @@
-console.log('Client side javascript file is loaded!')
-
-
-
-
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
@@ -15,7 +9,7 @@ weatherForm.addEventListener('submit', (e) =>{
     const location = search.value;
 
     
-    messageOne.textContent = 'Loading...'
+    messageOne.textContent = 'Loading..'
 
     fetch('http://localhost:3000/weather?address=' + location).then((response) =>{
     response.json().then((data) =>{
