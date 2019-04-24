1aa81e178979b8709a555e76f19dc35bc8a0811e
Vladzimir Yasinsky <yasinsky2015@gmail.com>
2019-04-25 00:36:25 +0300

    connect heroku

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ const hbs = require('hbs')
 const forecast = require('./utils/forecast')
 const geocode = require('./utils/geocode')
 
+const port = process.env.PORT || 3000
+
 const app = express()
 
 // Define paths for Express config
@@ -98,6 +100,6 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000.')
+app.listen(port, () => {
+    console.log('Server is up on port ' + port)
 })
